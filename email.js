8afb7c94186db89082f81f3a11013b129a9d1cd5
Your Name <ZGoldman@protonmail.com>
2018-06-21 19:24:28 -0700

    first commit

---
+++ email.js
@@ -0,0 +1,15 @@
+//Name of the file : email.js
+// using SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+require('dotenv').config();
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+    to: 'ZGoldman@protonmail.com',					//receiver's email
+    from: 'test@example3.com',			//sender's email
+    subject: 'SendGrid is working!!',				//Subject
+    text: 'and finally  i can send email from sendgrid',		//content
+    html: 'and easy to do anywhere, even with Node.js',			//HTML content
+};
+sgMail.send(msg);
---
+++ server.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+//var profile = require('./profile')
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+//app.use('/profile', profile)
+
+app.set('views', './views');
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    const data = {
+        person: {
+            firstName: 'Zach',
+            lastName: 'Goldman',
+        }
+    }
+
+    // Notice now the data is the second argument passed to the template render method
+    res.render('index', data);
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.post('/thanks', (req, res) => {
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: 'ZGoldman@protonmail.com',					//receiver's email
+        from: req.body.email,			//sender's email
+        subject: 'A new contact request via SendGrid',				//Subject
+        text: req.body.firstName + " " + req.body.lastName + ": " + req.body.textarea,		//content
+        html: req.body.firstName + " " + req.body.lastName + ": " + req.body.textarea,			//HTML content
+    };
+    sgMail.send(msg);
+
+    res.render('thanks', { contact: req.body })
+});
+
+app.listen(8080, () => {
+    console.log('listening at http://localhost:8080');
+});
